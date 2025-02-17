a5656fb9d7602b555fd9179c28f24bc349db85ec
gabrielrauda <gabrielrauda.adoc@gmail.com>
2025-02-16 21:08:39 -0600

    Update EjemploList.java

--- src/Ejemplos/EjemploList.java
+++ src/Ejemplos/EjemploList.java
@@ -1,34 +1,37 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class EjemploList {
     public static void main(String[] args) {
-        // Se declara el list
-        List<String> nombres = new ArrayList<>();
+        // para declarar la lista
+        List<String> empleados = new ArrayList<>();
 
-        // Como agregamos elementos a la lista
-        nombres.add("Juan Perez");
-        nombres.add("Maria Martinez");
-        nombres.add("Carlos Hernandez");
+        // agregar elementos
+        empleados.add("Juan Perez");
+        empleados.add("Julissa Roque");
+        empleados.add("Carlos Lopez");
+        empleados.add("Sofia Rivera");
+        empleados.add("Jose Cortez");
+        empleados.add("Maria Martinez");
+        empleados.add("Carlos Hernandez");
 
-        // Mostramos la lista creada
-        System.out.println("Lista inicial: " + nombres);
+        // Mostrar lista
+        System.out.println("Lista inicial: " + empleados);
 
         // Mostramos un elemento de la lista por indice
-        String nombre = nombres.get(0);
-        System.out.println("Elemento en indice 1: " + nombre);
+        String empleado = empleados.get(0);
+        System.out.println("Elemento en no. 1: " + empleado);
 
-        // Como se eliminan los elementos de la lista
-        nombres.remove(0);
-        System.out.println("Lista después de eliminar el primer elemento: " + nombres);
+        // Eliminar elemento por indice
+        empleados.remove(0);
+        System.out.println("Lista después de eliminar el primer elemento: " + empleados);
 
-        // Eliminar un elemento por valor
-        nombres.remove("Carlos Hernandez");
-        System.out.println("Lista después de eliminar 'Carlos Hernandez': " + nombres);
+        // Eliminar elemento por valor
+        empleados.remove("Carlos Hernandez");
+        System.out.println("Lista después de eliminar 'Carlos Hernandez': " + empleados);
 
-        // Procedemos a verificar si un elemento existe en la lista
-        boolean existe = nombres.contains("Maria Martinez");
+        // verificar si un elemento existe en la lista
+        boolean existe = empleados.contains("Maria Martinez");
         System.out.println("¿La lista contiene a 'Maria Martinez'?: " + existe);
     }
 }
